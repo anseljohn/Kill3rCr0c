0576fb5754b44034d6a3d49219ced52c426a4409
John Anselmo <jjohnanselmo@gmail.com>
2017-10-01 12:48:09 -0400

    PermitRootLogin change addition

--- Ubuntu Scripts/2killerssh.sh
+++ Ubuntu Scripts/2killerssh.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sed -i 's/PermitRootLogin\ yes/PermitRootLogin\ no/' /etc/ssh/sshd_config
+sed -i 's/PermitRootLogin\ prohibit-password/PermitRootLogin\ no/' /etc/ssh/sshd_config
 sed -i 's/PermitRootLogin\ without_password/PermitRootLogin\ no/' /etc/ssh/sshd_config
 sed -i 's/PermitEmptyPasswords\ yes/PermitEmptyPasswords\ no/' /etc/ssh/sshd_config
