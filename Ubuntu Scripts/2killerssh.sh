d9f10b4ce90bacbea10bfe30033fdbc5541a0770
John Anselmo <jjohnanselmo@gmail.com>
2017-09-30 21:14:30 -0400

    Fixed gufw conf and accidental script running prevention

---
+++ Ubuntu Scripts/2killerssh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sed -i 's/PermitRootLogin\ yes/PermitRootLogin\ no/' /etc/ssh/sshd_config
+sed -i 's/PermitRootLogin\ without_password/PermitRootLogin\ no/' /etc/ssh/sshd_config
+sed -i 's/PermitEmptyPasswords\ yes/PermitEmptyPasswords\ no/' /etc/ssh/sshd_config
