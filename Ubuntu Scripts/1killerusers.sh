502dfaecd858eb4cf1c98ed17180891912b618a8
johnanselmo-s3i <jjohnanselmo@gmail.com>
2017-10-20 12:19:17 -0400

    Working USERDEL propter

--- Ubuntu Scripts/1killerusers.sh
+++ Ubuntu Scripts/1killerusers.sh
@@ -3,16 +3,20 @@
 l=$(grep "^UID_MIN" /etc/login.defs)
 l1=$(grep "^UID_MAX" /etc/login.defs)
 
-users=$(awk -F':' -v "min=${l##UID_MIN}" -v "max=${l1##UID_MAX}" '{ if ( $3 >= min && $3 <= max ) print $0}' /etc/passwd)
+users=$(awk -F':' -v "min=${l##UID_MIN}" -v "max=${l1##UID_MAX}" '{ if ( $3 >= min && $3 <= max ) print $1}' /etc/passwd)
 set -f
 userArray=(${users// /})
+isRunning=true;
 
-for names in "${userArray[@]}"
-do
-    read -p "Remove $names?" yn
+function prompt {
+  for names in "${userArray[@]}"
+  do
+    read -p "Remove $names? [Yy/Nn]  " -r yn
     case $yn in
-        [Yy]* ) deluser #; break;;
-        [Nn]* ) 
-        * ) echo "Please answer yes or no.";;
+      [Yy]* ) userdel $names;;
+      [Nn]* ) ;;
+      * ) echo "Please enter Yes or No"; prompt;;
     esac
-done
+  done
+}
+prompt
