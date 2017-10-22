7f1417d885189e8f1fbcb1415010a4594a128f45
John Anselmo <jjohnanselmo@gmail.com>
2017-10-21 20:24:32 -0400

    Useless echo

--- Ubuntu Scripts/1killerusers.sh
+++ Ubuntu Scripts/1killerusers.sh
@@ -4,7 +4,6 @@ l=$(grep "^UID_MIN" /etc/login.defs)
 l1=$(grep "^UID_MAX" /etc/login.defs)
 
 users=$(awk -F':' -v "min=${l##UID_MIN}" -v "max=${l1##UID_MAX}" '{ if ( $3 >= min && $3 <= max ) print $1}' /etc/passwd)
-echo $users
 set -f
 userArray=(${users// /})
 isRunning=true;
