58a76a2fd972dcb81bdaafa05c0503538bb32180
John Anselmo <jjohnanselmo@gmail.com>
2018-01-12 17:16:24 -0500

    ssh update

--- Ubuntu Scripts/2-ssh.sh
+++ Ubuntu Scripts/2-ssh.sh
@@ -2,6 +2,6 @@
 
 sed -i 's/PermitRootLogin\ yes/PermitRootLogin\ no/' /etc/ssh/sshd_config
 sed -i 's/^Protocol 1 2/Protocol 2/' /etc/ssh/ssh_config
-# sed -i 's/PermitRootLogin\ prohibit-password/PermitRootLogin\ no/' /etc/ssh/sshd_configss
-# sed -i 's/PermitRootLogin\ without_password/PermitRootLogin\ no/' /etc/ssh/sshd_config
-# sed -i 's/PermitEmptyPasswords\ yes/PermitEmptyPasswords\ no/' /etc/ssh/sshd_config
+sed -i 's/PermitRootLogin\ prohibit-password/PermitRootLogin\ no/' /etc/ssh/sshd_configss
+sed -i 's/PermitRootLogin\ without_password/PermitRootLogin\ no/' /etc/ssh/sshd_config
+sed -i 's/PermitEmptyPasswords\ yes/PermitEmptyPasswords\ no/' /etc/ssh/sshd_config
