148f714b1c901420f2eeb0756e7a6a1db6435608
John Anselmo <jjohnanselmo@gmail.com>
2018-02-09 15:14:47 -0500

    Call file update

--- Ubuntu Scripts/0killercall.sh
+++ Ubuntu Scripts/0killercall.sh
@@ -30,11 +30,11 @@
 # chmod 777 1-gufw.sh
 # ./1-gufw.sh
 
-
+#TESTED
 # chmod 777 1-bum.sh
 # ./1-bum.sh
 
-
+#TESTED
 # chmod 777 1-auditd.sh
 # ./1-auditd.sh
 
@@ -46,29 +46,38 @@
 # chmod 777 2-ssh.sh
 # ./2-ssh.sh
 
-
+#TESTED
 # chmod 777 2-ftp.sh
 # ./2-ftp.sh
 
-
+#TESTED
 # chmod 777 2-lightdm.sh
 # ./2-ligthdm.sh
 
-
+#TESTED
 # chmod 777 2-ipv4warding.sh
 # ./2-ipv4warding.sh
 
 
-# chmod 777 2-passwd.sh
-# ./2-passwd.sh
+# chmod 777 2-commonpassword.sh
+# ./2-commonpassword.sh
 
-
+#TESTED
 # chmod 777 2-logindefs.sh
 # ./2-logindefs.sh
 
-
 # chmod 777 2-commonauth.sh
 # ./2-commonauth.sh
+
+#TESTED
+# chmod 777 3-changepasswords.sh
+# ./3-changepasswords.sh
+
+#TESTED
+# chmod 777 3-rootpass.sh
+# ./3-rootpass.sh
+
+
 
 apt-get update -y --force-yes
 apt-get upgrade -y --force-yes
