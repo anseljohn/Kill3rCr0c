365516fa617ae0ce6b621c1517b389db0edd0b6f
John Anselmo <jjohnanselmo@gmail.com>
2018-02-10 10:34:02 -0800

    REGEX Changes

--- Ubuntu Scripts/2-logindefs.sh
+++ Ubuntu Scripts/2-logindefs.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-sed -i -e 's/PASS_MAX_DAYS  99999/PASS_MAX_DAYS  90/g' /etc/login.defs
-sed -i -e 's/PASS_MIN_DAYS  0/PASS_MIN_DAYS  10/g' /etc/login.defs
+sed -i '/PASS_MAX_DAYS/c\PASS_MAX_DAYS  90' /etc/login.defs
+sed -i '/PASS_MIN_DAYS/c\PASS_MIN_DAYS  10' /etc/login.defs
+
+sed -i '/PASS_MAX_DAYS/{s/.*/#&/;:A;n;bA}' /etc/login.defs
+sed -i '/PASS_MIN_DAYS/{s/.*/#&/;:A;n;bA}' /etc/login.defs
