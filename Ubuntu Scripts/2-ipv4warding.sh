8e92d2c951277a8fdd1206ee0c0b0e034b902ada
John Anselmo <jjohnanselmo@gmail.com>
2018-02-10 12:36:49 -0800

    REGEX modification

--- Ubuntu Scripts/2-ipv4warding.sh
+++ Ubuntu Scripts/2-ipv4warding.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sed -i -e 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=0/g' /etc/sysctl.conf
+sed -i '/^#.*net.ipv4.ip_forward/s/^#//' /etc/sysctl.conf
+sed -i -e 's/net.ipv4.ip_forward=1/net.ipv4.ip_forward=0/g' /etc/sysctl.conf
