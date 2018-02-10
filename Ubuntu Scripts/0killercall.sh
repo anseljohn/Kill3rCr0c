e7305e53e6175ab2678c715ff104bbc495e5ecb2
John Anselmo <jjohnanselmo@gmail.com>
2018-02-10 12:44:17 -0800

    REGEX Changes

--- Ubuntu Scripts/0killercall.sh
+++ Ubuntu Scripts/0killercall.sh
@@ -71,12 +71,12 @@ chmod 777 2-ftp.sh
 chmod 777 2-lightdm.sh
 ./2-ligthdm.sh
 
+#TESTED
+chmod 777 2-ipv4warding.sh
+./2-ipv4warding.sh
+
 ######################################################
 #END OF AUTO RUN SCRIPTS
-
-#TESTED
-# chmod 777 2-ipv4warding.sh
-# ./2-ipv4warding.sh
 
 #NOT TESTED
 # chmod 777 2-commonpassword.sh
