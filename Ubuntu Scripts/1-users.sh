1767d4ad9e07e7bc81db1bdacb5a1989c6cac240
John Anselmo <jjohnanselmo@gmail.com>
2018-02-10 09:35:47 -0800

    Merge add/remove users

--- Ubuntu Scripts/1-users.sh
+++ Ubuntu Scripts/1-users.sh
@@ -6,9 +6,8 @@ l1=$(grep "^UID_MAX" /etc/login.defs)
 users=$(awk -F':' -v "min=${l##UID_MIN}" -v "max=${l1##UID_MAX}" '{ if ( $3 >= min && $3 <= max ) print $1}' /etc/passwd)
 set -f
 userArray=(${users// /})
-isRunning=true;
 
-function prompt {
+function rem {
   for names in "${userArray[@]}"
   do
     read -p "Remove $names? [Yy/Nn]  " -r yn
@@ -18,5 +17,33 @@ function prompt {
       * ) echo "Please enter Yes or No"; prompt;;
     esac
   done
+}
+
+function add {
+  while true
+  do
+      echo
+      read -p "Username: " -r resp
+      read -p "Add user: $resp ? [Yy/Nn]>>  " -r yn
+      case $yn in
+          [Yy]* ) 
+            adduser --force-badname $resp
+            echo -ne "\n*\n*\n*\nUser \"$resp\" has been added\n"
+            ;;
+          [Nn]* ) ;;
+          * ) 
+            echo -ne "Please enter Yes or No [Yy/Nn]\n"
+            add
+            ;;
+      esac
+  done
+}
+
+function prompt {
+  read -p "Remove or add users? [Rr/Aa]  " -r ra
+  case $ra in
+    [Rr]* ) rem;;
+    [Aa]* ) add;;
+  esac
 }
 prompt
