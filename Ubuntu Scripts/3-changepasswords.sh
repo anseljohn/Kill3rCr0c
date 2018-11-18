94a619b3b25e268e1707eb4f9bd1c7a7ad99f81b
johnanselmo-s3i <jjohnanselmo@gmail.com>
2018-11-18 11:44:27 -0800

    Cleanup!

--- Ubuntu Scripts/3-changepasswords.sh
+++ Ubuntu Scripts/3-changepasswords.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-l=$(grep "^UID_MIN" /etc/login.defs)
-l1=$(grep "^UID_MAX" /etc/login.defs)
-
-users=$(awk -F':' -v "min=${l##UID_MIN}" -v "max=${l1##UID_MAX}" '{ if ( $3 >= min && $3 <= max ) print $1}' /etc/passwd)
-set -f
-userArray=(${users// /})
-
-for names in "${userArray[@]}"
-do
+for names in $(cat < "users.txt"); do
    isAdmin=false
    doIFS=$'\n'
    set -f
