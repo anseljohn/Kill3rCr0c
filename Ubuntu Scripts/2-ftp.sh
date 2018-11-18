a7a681b706b3afda36bf272aa18d75d6545582da
johnanselmo-s3i <jjohnanselmo@gmail.com>
2018-11-18 10:51:12 -0800

    Unsecure ftp config

--- Ubuntu Scripts/2-ftp.sh
+++ Ubuntu Scripts/2-ftp.sh
@@ -3,5 +3,5 @@
 apt-get install vsftpd
 
 sed -i '/^#.*write_enable=YES/s/^#//' /etc/vsftpd.conf
-echo "allow_writable_chroot=YES" >> /etc/vsftpd.conf
+echo "allow_writable_chroot=NO" >> /etc/vsftpd.conf
 sed -i '/^#.*chroot_local_user=YES/s/^#//' /etc/vsftpd.conf
