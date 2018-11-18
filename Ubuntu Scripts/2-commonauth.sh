4da02205a320eb0e6f3b41a7dd9d69b3d6cd32f3
johnanselmo-s3i <jjohnanselmo@gmail.com>
2018-11-18 10:48:54 -0800

    Update parameters

--- Ubuntu Scripts/2-commonauth.sh
+++ Ubuntu Scripts/2-commonauth.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apt-get install libpam-cracklib -y --force-yes
-echo "auth optional pam_tally2.so deny=4 onerr=fail unlock_time=900 audit even_deny_root_root_account silent" >> /etc/pam.d/common-auth
+echo "auth optional pam_tally2.so deny=8 onerr=fail unlock_time=30 audit root_unlock_time=30 silent" >> /etc/pam.d/common-auth
