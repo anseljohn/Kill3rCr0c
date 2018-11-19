41f5968a9f1078707c50a63043476da0bdcb0516
johnanselmo-s3i <jjohnanselmo@gmail.com>
2018-11-19 14:23:51 -0800

    Validation

--- Ubuntu Scripts/1-users.sh
+++ Ubuntu Scripts/1-users.sh
@@ -12,58 +12,27 @@ for name in "${userArray[@]}"; do
   isValid=false;
   doIFS=$"\n"
   set -f
-  for $validName in $(cat < "users.txt"); do
-    if [$name = $validName]; then
+  for validName in $(cat < "users.txt"); do
+    if [ $name = $validName ] ; then
       isValid=true
     fi
   done
-  if [isValid = false]; then
+  if [ "$isValid" = false ] ; then
     userdel $name
   fi
 done
 
-#check to see if valid user exists, if not, add
+# check to see if valid user exists, if not, add
 doIFS=$"\n"
 set -f
-for $validName in $(cat < "users.txt"); do
+for validName in $(cat < "users.txt") ; do
   isExisting=false
-  for name in "${userArray[@]}"; do
-    if [$validName = $name]; then
-      isExisting=true;
+  for name in "${userArray[@]}" ; do
+    if [ $validName = $name ]; then
+      isExisting=true
     fi
   done
-  if [isExisting = false]; then
-    useradd newuser; echo -e "passwdofuser\npasswdofuser" | passwd newuser
+  if [ "$isExisting" = false ] ; then
+    echo -e "123qwe!@#QWE\n123qwe!@#QWE\n\n\n\n\ny" | adduser $validName
   fi
 done
-
-
-# function add {
-#   while true
-#   do
-#       echo
-#       read -p "Username: " -r resp
-#       read -p "Add user: $resp ? [Yy/Nn]>>  " -r yn
-#       case $yn in
-#           [Yy]* ) 
-#             adduser --force-badname $resp
-#             echo -ne "\n*\n*\n*\nUser \"$resp\" has been added\n"
-#             ;;
-#           [Nn]* ) ;;
-#           * ) 
-#             echo -ne "Please enter Yes or No [Yy/Nn]\n"
-#             add
-#             ;;
-#       esac
-#   done
-# }
-
-# function prompt {
-#   read -p "Remove or add users? [Rr/Aa]  " -r ra
-#   case $ra in
-#     [Rr]* ) rem;;
-#     [Aa]* ) add;;
-#   esac
-# }
-# prompt
-
