7391f7de77e0eecd6ff83f1d848b0537493bd1d1
johnanselmo-s3i <jjohnanselmo@gmail.com>
2018-11-19 14:04:44 -0800

    Validate users, add valid users

--- Ubuntu Scripts/1-users.sh
+++ Ubuntu Scripts/1-users.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+# # !/bin/bash
 
 l=$(grep "^UID_MIN" /etc/login.defs)
 l1=$(grep "^UID_MAX" /etc/login.defs)
@@ -7,43 +7,63 @@ users=$(awk -F':' -v "min=${l##UID_MIN}" -v "max=${l1##UID_MAX}" '{ if ( $3 >= m
 set -f
 userArray=(${users// /})
 
-function rem {
-  for names in "${userArray[@]}"
-  do
-    read -p "Remove $names? [Yy/Nn]  " -r yn
-    case $yn in
-      [Yy]* ) userdel $names && rm -rf /home/$names; echo "User \"$names\" has been deleted";;
-      [Nn]* ) ;;
-      * ) echo "Please enter Yes or No"; prompt;;
-    esac
+# check to see if user is a valid user, if not, remove
+for name in "${userArray[@]}"; do
+  isValid=false;
+  doIFS=$"\n"
+  set -f
+  for $validName in $(cat < "users.txt"); do
+    if [$name = $validName]; then
+      isValid=true
+    fi
   done
-}
+  if [isValid = false]; then
+    userdel $name
+  fi
+done
 
-function add {
-  while true
-  do
-      echo
-      read -p "Username: " -r resp
-      read -p "Add user: $resp ? [Yy/Nn]>>  " -r yn
-      case $yn in
-          [Yy]* ) 
-            adduser --force-badname $resp
-            echo -ne "\n*\n*\n*\nUser \"$resp\" has been added\n"
-            ;;
-          [Nn]* ) ;;
-          * ) 
-            echo -ne "Please enter Yes or No [Yy/Nn]\n"
-            add
-            ;;
-      esac
+#check to see if valid user exists, if not, add
+doIFS=$"\n"
+set -f
+for $validName in $(cat < "users.txt"); do
+  isExisting=false
+  for name in "${userArray[@]}"; do
+    if [$validName = $name]; then
+      isExisting=true;
+    fi
   done
-}
+  if [isExisting = false]; then
+    useradd newuser; echo -e "passwdofuser\npasswdofuser" | passwd newuser
+  fi
+done
 
-function prompt {
-  read -p "Remove or add users? [Rr/Aa]  " -r ra
-  case $ra in
-    [Rr]* ) rem;;
-    [Aa]* ) add;;
-  esac
-}
-prompt
+
+# function add {
+#   while true
+#   do
+#       echo
+#       read -p "Username: " -r resp
+#       read -p "Add user: $resp ? [Yy/Nn]>>  " -r yn
+#       case $yn in
+#           [Yy]* ) 
+#             adduser --force-badname $resp
+#             echo -ne "\n*\n*\n*\nUser \"$resp\" has been added\n"
+#             ;;
+#           [Nn]* ) ;;
+#           * ) 
+#             echo -ne "Please enter Yes or No [Yy/Nn]\n"
+#             add
+#             ;;
+#       esac
+#   done
+# }
+
+# function prompt {
+#   read -p "Remove or add users? [Rr/Aa]  " -r ra
+#   case $ra in
+#     [Rr]* ) rem;;
+#     [Aa]* ) add;;
+#   esac
+# }
+# prompt
+
