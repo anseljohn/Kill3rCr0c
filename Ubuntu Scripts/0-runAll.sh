beb46a5088c14c7b499d9bfa63ffe58b85768278
johnanselmo-s3i <jjohnanselmo@gmail.com>
2018-11-20 07:51:55 -0800

    Call file for all scripts

--- Ubuntu Scripts/0-runAll.sh
+++ Ubuntu Scripts/0-runAll.sh
@@ -1,4 +1,35 @@
 #!/bin/bash
 
-# add permissions to all scripts
-chmod 777 *
+./1-auditd.sh
+
+./1-browsers.sh
+
+./1-bum.sh
+
+./1-gufw.sh
+
+./1-remover.sh
+
+./1-users.sh
+
+./1-verification.sh
+
+./2-commonauth.sh
+
+./2-commonpassword.sh
+
+./2-firewall.sh
+
+./2-ftp.sh
+
+./2-ipv4warding.sh
+
+./2-lightdm.sh
+
+./2-logindefs.sh
+
+./2-ssh.sh
+
+./3-changepasswords.sh
+
+./3-rootpass.sh
